a707d91392322a9128a3bf8b8509624c4beed50b
Daniel Litt <daniel_litt@comcast.net>
2021-06-03 10:51:20 -0400

    Setup button that increments counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-hello-world';
+  count: number = 0;
+
+  incrementCounter(): void {
+    this.count++;
+  }
 }
